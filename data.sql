31de2252ec9ae7dd2eeefb408e2b67b506be8d32
lty2107 <rng48322@gmail.com>
2023-02-26 23:44:24 +0300

    База данных и системные файлы

--- data.sql
+++ data.sql
@@ -15,4 +15,6 @@ CREATE TABLE IF NOT EXISTS transaction_history
     FOREIGN KEY(from_id) REFERENCES wallet(id)
 );
 
-insert into wallet (address, balance) VALUES ('e240d825d255af751f5f55af8d9671beabdf2236c0a3b4e2639b3e182d994c88', 3.50)
+insert into wallet (address, balance) VALUES ('e240d825d255af751f5f55af8d9671beabdf2236c0a3b4e2639b3e182d994c88', 3.50);
+
+insert into wallet (address, balance) VALUES ('e240d825d255af751f5f55af8d9671beabdf389123ewqedq34dd3q231efwer23', 2.00)
