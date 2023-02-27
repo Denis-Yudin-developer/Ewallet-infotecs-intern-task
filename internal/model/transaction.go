9d0bff0d25e4890b1e994c320cbbfa104ba95938
lty2107 <rng48322@gmail.com>
2023-02-27 21:10:04 +0300

    Изменил поле в json на amount

--- internal/model/transaction.go
+++ internal/model/transaction.go
@@ -8,6 +8,6 @@ type Transaction struct {
 	ID             int64     `json:"id"`
 	FromAddress    string    `json:"from"`
 	ToAddress      string    `json:"to"`
-	TransferAmount float64   `json:"transfer_amount"`
+	TransferAmount float64   `json:"amount"`
 	TransferTime   time.Time `json:"transfer_time"`
 }
